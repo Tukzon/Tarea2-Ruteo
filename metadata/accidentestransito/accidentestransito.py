443cc7f2cb17e7a157872d8368f09e9371773c14
Gonzalo Salazar <gonsalazarm@gmail.com>
2023-11-24 03:26:43 -0300

    cambiado accidentes, añadido apt para geojson

--- metadata/accidentestransito/accidentestransito.py
+++ metadata/accidentestransito/accidentestransito.py
@@ -1,10 +1,16 @@
 import requests
 import json
+import os
 
 def get_accidentes_transito():
-    url = "http://www.geoportal.cl/arcgis/rest/services/MinisteriodeTransporteyTelecomunicaciones/chile_mtt_conaset_accidente_pts_criticos_2019/MapServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+    #url = "http://www.geoportal.cl/arcgis/rest/services/MinisteriodeTransporteyTelecomunicaciones/chile_mtt_conaset_accidente_pts_criticos_2019/MapServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+    url = "https://opendata.arcgis.com/api/v3/datasets/1f03237a14684ac280906f9ca6166d23_0/downloads/data?format=geojson&spatialRefId=4326&where=1=1"
     r = requests.get(url)
     data = json.loads(r.text)
     return data
 
-print(get_accidentes_transito())
+def save_json(data):
+    with open('./accidentes_transito.json', 'w') as file:
+        json.dump(data, file)
+
+save_json(get_accidentes_transito())
